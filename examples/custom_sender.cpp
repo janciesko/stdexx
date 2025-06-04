9eaab96b7368bed11cf06e254e8312fce64fc23d
Jan Ciesko <jan.ciesko@gmail.com>
2025-06-04 13:42:49 -0600

    Add descriptions of sender's anatomy
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>

--- examples/custom_sender.cpp
+++ examples/custom_sender.cpp
@@ -12,6 +12,13 @@ struct my_sender {
   using completion_signatures = stdexec::
     completion_signatures<stdexec::set_value_t(int)>;
 
+  /*
+  - Op state is templated on receiver (callback)
+  - Has start
+  - Calls set_value on (rcv, value) synchronously 
+  - This uses compatible completion signature to provide value to rcv
+  - This will invoke scheduler to invoke next sender (the recv is the glue)
+  */
   template <class Receiver>
   struct op {
     Receiver rcv;
@@ -21,6 +28,13 @@ struct my_sender {
     }
   };
 
+  /*
+  - Scheduler invokes sender via the tag invoke
+  - Invoke accepts args: connect_t, self, and rcv (rcv is provided by the sender adapter)
+  - Returns an op state intance, initialized with rcv. 
+  - ! The initialization of op sets of the rcv (callback)
+  - ! The callback is called on completion of the op::start
+  */
   template <class Receiver>
   friend auto tag_invoke(stdexec::connect_t, my_sender, Receiver rcv) -> op<Receiver> {
     return {std::move(rcv)};
@@ -29,8 +43,8 @@ struct my_sender {
 };
 
 auto main() -> int {
-  auto algorithm = my_sender{};
-  auto [a] = stdexec::sync_wait(std::move(algorithm)).value();
+  auto my_sender = my_sender{};
+  auto [a] = stdexec::sync_wait(std::move(my_sender)).value();
   return (a == 42) ? 1: 0;
 }
 
--- examples/fibonacci.cpp
+++ examples/fibonacci.cpp
@@ -49,6 +49,13 @@ struct fib_s {
   long n;
   Scheduler sched;
 
+  /*
+  - Op state is templated on receiver (callback)
+  - Has start
+  - Calls set_value on (rcv, value) synchronously 
+  - This uses compatible completion signature to provide value to rcv
+  - This will invoke scheduler to invoke next sender (the recv is the glue)
+  */
   template <class Receiver>
   struct operation {
     Receiver rcvr_;
@@ -73,6 +80,13 @@ struct fib_s {
     }
   };
 
+  /*
+  - Scheduler invokes sender via the tag invoke
+  - Invoke accepts args: connect_t, self, and recv (recv is provided by the sender adapter)
+  - Returns an op state intance, initialized with rcv. 
+  - ! The initialization of op sets of the rcv (callback)
+  - ! The callback is called on completion of the op::start
+  */
   template <stdexec::receiver_of<completion_signatures> Receiver>
   friend auto tag_invoke(stdexec::connect_t, fib_s self, Receiver rcvr) -> operation<Receiver> {
     return {static_cast<Receiver&&>(rcvr), self.cutoff, self.n, self.sched};
